9737e32cfb7a71d5f93aa15a13502852b2cbf02b
Crashbadger24 <h075vineet@gmail.com>
2025-03-12 10:20:12 +0530

    Commiting codes

---
+++ contracts/sample1.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Walletspend {
+    uint public wallet=500;
+    string public notifySpend="you have spent money";
+    bool public spend =false;
+
+
+}
